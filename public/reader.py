a5ab3c8a8cbf0f339f59155a2e9bdb4937230128
Svoykina N <nadya.svojkina@gmail.com>
2017-12-05 04:23:08 +0100

    csv reader

---
+++ public/reader.py
@@ -0,0 +1,9 @@
+import csv
+
+def reader(filename):
+    matrix = []
+    for r in open(filename):
+        r = r.strip().split(",")
+        matrix.append(list(map(lambda x: float(x.strip()), r)))
+
+    return matrix
