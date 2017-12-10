c6ddc6ecca797803d14b5592af74757a1d0af0e3
Svoykina N <nadya.svojkina@gmail.com>
2017-12-10 03:06:06 +0300

    Added custom user output files

--- markov_clustering.py
+++ markov_clustering.py
@@ -13,18 +13,22 @@ if __name__ == '__main__':
         filename = sys.argv[1]
     except:
         raise Exception('Забыли ввести имя файла на вход!')
+
     matrix = reader(filename)
 
     begin_time = time.time()
     matrix, clusters, loops_count = mcl(matrix)
     end_time = time.time()
 
-    with open('output_info.txt', 'wt') as output_file:
+    output_info = sys.argv[2] if len(sys.argv) > 2 else 'output_info.txt'
+    output_graph = sys.argv[3] if len(sys.argv) > 3 else 'output_graph.csv'
+
+    with open(output_info, 'wt') as output_file:
         output_file.write("Вершин в графе: {}\n".format(len(matrix[0])))
         output_file.write("Посчитано за {} секунд\n".format(end_time - begin_time))
         output_file.write("Программа сработала за {} циклов\n".format(loops_count))
         clusters_to_output(clusters, output_file)
 
-    with open('output_graph.csv', 'wt') as output_file:
+    with open(output_graph, 'wt') as output_file:
         writer = csv.writer(output_file)
         writer.writerows(matrix)
